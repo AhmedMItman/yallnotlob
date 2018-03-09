3eb3e746ae24cca44b1a12386cf41235525822b3
Marei Morsy <mareimorsy@yahoo.com>
2018-03-10 00:34:54 +0200

    adding assets and master layout

--- yallanolob/config/initializers/assets.rb
+++ yallanolob/config/initializers/assets.rb
@@ -12,3 +12,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( bootstrap.css font-awesome.css style.css jquery.js cable.js bootstrap.js app.js )
+# Rails.root.join("app", "assets", "fonts")
--- yallanolob/config/routes.rb
+++ yallanolob/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'friends/index'
+
+  get 'home/index'
+  get 'friends/', to: 'friends#index'
+
   resources :notifications
   resources :user_notifications
   resources :items
@@ -6,6 +11,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :groups
   resources :users
-  root to: "users#index"
+  root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
